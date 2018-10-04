02c901c8af3b6d4281a62af98e58f67facb04d2b
Harshad Deo <subterranean.hominid@gmail.com>
2018-10-04 19:25:45 +0530

    add quantile function

--- js/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
+++ js/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
@@ -1,8 +1,10 @@
 package simianquant.mathbridge
 
-object NormalDistribution {
+object NormalDistribution extends NormalDistributionInterface {
   private final val sqrt2 = math.sqrt(2)
 
   def cdf(x: Double): Double = (1.0 - MathJSFacade.erf(-x / sqrt2)) / 2.0
+
+  def quantile(x: Double): Double = -sqrt2 * InverseErrorFunction(1 - 2 * x)
 
 }
--- jvm/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
+++ jvm/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
@@ -2,10 +2,12 @@ package simianquant.mathbridge
 
 import org.apache.commons.math3.distribution.{NormalDistribution => ND}
 
-object NormalDistribution {
+object NormalDistribution extends NormalDistributionInterface {
   @SuppressWarnings(Array("org.wartremover.warts.Null"))
   private val nd = new ND(null, 0, 1)
 
-  def cdf(x: Double): Double = nd.cumulativeProbability(x)
+  override final def cdf(x: Double): Double = nd.cumulativeProbability(x)
+
+  override final def quantile(x: Double): Double = nd.inverseCumulativeProbability(x)
 
 }
