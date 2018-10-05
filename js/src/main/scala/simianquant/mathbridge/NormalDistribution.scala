bb3f1a22960b9e8a358f4a54cd1a430a87f8f9a5
Harshad Deo <subterranean.hominid@gmail.com>
2018-10-05 09:15:24 +0530

    norm dist uses erf

--- js/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
+++ js/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
@@ -3,8 +3,8 @@ package simianquant.mathbridge
 object NormalDistribution extends NormalDistributionInterface {
   private final val sqrt2 = math.sqrt(2)
 
-  def cdf(x: Double): Double = (1.0 - MathJSFacade.erf(-x / sqrt2)) / 2.0
+  override final def cdf(x: Double): Double = (1.0 - MathJSFacade.erf(-x / sqrt2)) / 2.0
 
-  def quantile(x: Double): Double = -sqrt2 * InverseErrorFunction(1 - 2 * x)
+  override final def quantile(x: Double): Double = -sqrt2 * InverseErrorFunction(1 - 2 * x)
 
 }
--- jvm/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
+++ jvm/src/main/scala/simianquant/mathbridge/NormalDistribution.scala
@@ -1,13 +1,13 @@
 package simianquant.mathbridge
 
-import org.apache.commons.math3.distribution.{NormalDistribution => ND}
+import org.apache.commons.math3.special.Erf
 
 object NormalDistribution extends NormalDistributionInterface {
-  @SuppressWarnings(Array("org.wartremover.warts.Null"))
-  private val nd = new ND(null, 0, 1)
+  private val M_SQRT2 = math.sqrt(2)
+  private val M_SQRT1_2 = 1 / math.sqrt(2)
 
-  override final def cdf(x: Double): Double = nd.cumulativeProbability(x)
+  override final def cdf(x: Double): Double = 0.5 * Erf.erfc(-x * M_SQRT1_2)
 
-  override final def quantile(x: Double): Double = nd.inverseCumulativeProbability(x)
+  override final def quantile(x: Double): Double = -M_SQRT2 * Erf.erfcInv(2 * x)
 
 }
