dddb8d23d51651630a1cc961719e10687044bed9
Harshad Deo <subterranean.hominid@gmail.com>
2018-12-20 12:51:33 +0530

    add RationalApi

---
+++ shared/src/test/scala/simianquant/test/mathbridge/RationalApi.scala
@@ -0,0 +1,16 @@
+package simianquant.mathbridge
+
+import spire.std.AnyInstances
+import spire.syntax.AllSyntax
+
+/** Utility type to club all imports required to use [[spire.Rational]]
+  *
+  * @author Harshad Deo
+  * @since 0.2.0
+  */
+trait RationalApi extends AnyInstances with AllSyntax {
+  type Rational = spire.math.Rational
+  val Rational = spire.math.Rational
+}
+
+object RationalApi extends RationalApi
