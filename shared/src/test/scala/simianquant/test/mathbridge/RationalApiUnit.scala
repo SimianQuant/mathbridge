b2b5c037121bc88ea131874146cbdff1f09c27e1
Harshad Deo <subterranean.hominid@gmail.com>
2018-12-20 13:15:07 +0530

    add more test cases to rational API

--- shared/src/test/scala/simianquant/test/mathbridge/RationalApiUnit.scala
+++ shared/src/test/scala/simianquant/test/mathbridge/RationalApiUnit.scala
@@ -11,6 +11,19 @@ final class RationalApiUnit extends FlatSpec {
     assertCompiles("""val a: Rational = 1.2f""")
     assertCompiles("""val a: Rational = 1L""")
     assertCompiles("""Rational("3/4")""")
+
+    assertCompiles("""val a = Rational(1)""")
+    assertCompiles("""val a = Rational(1.2)""")
+    assertCompiles("""val a = Rational(1.2f)""")
+    assertCompiles("""val a = Rational(1L)""")
+    assertCompiles("""val a = Rational(2, 3)""")
+    assertCompiles("""val a = Rational(2, 3L)""")
+
+    assertCompiles("Rational(3) + Rational(2.3)")
+    assertCompiles("Rational(3) - Rational(2.3)")
+    assertCompiles("Rational(3) * Rational(2.3)")
+    assertCompiles("Rational(3, 5) / Rational(2.3)")
+
     assertTypeError("""val a: Rational = "foo" """)
   }
 
