ba0c763500bed1b36c7f16ffefee0bc3f150e4b2
Harshad Deo <subterranean.hominid@gmail.com>
2019-01-14 12:56:18 +0530

    bump version to 0.2.2-SNAPSHOT

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,7 +1,7 @@
 object Settings {
   object versions {
     val commonsMath: String = "3.6.1"
-    val project: String = "0.2.1"
+    val project: String = "0.2.2-SNAPSHOT"
     val scala: String = "2.12.6"
     val scalatest: String = "3.0.5"
     val scalacheck: String = "1.14.0"
