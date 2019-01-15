739b5626779ee65a37ffeb33a237d07f1cc7a80a
Harshad Deo <subterranean.hominid@gmail.com>
2019-01-15 21:44:39 +0530

    add spire complex double api operations tests

--- shared/src/test/scala/simianquant/test/mathbridge/SpireComplexDoubleApiUnit.scala
+++ shared/src/test/scala/simianquant/test/mathbridge/SpireComplexDoubleApiUnit.scala
@@ -2,6 +2,7 @@ package simianquant.test.mathbridge
 
 import org.scalatest.FlatSpec
 import simianquant.mathbridge.SpireComplexDoubleApi._
+import simianquant.mathbridge.{SpireComplexDoubleDelegate => SD}
 import spire.math.Complex
 
 final class SpireComplexDoubleApiUnit extends FlatSpec {
@@ -37,7 +38,7 @@ final class SpireComplexDoubleApiUnit extends FlatSpec {
     assert(SCD.polar(1.1, math.Pi / 5) == Complex.polar[Double](1.1, math.Pi / 5))
   }
 
-  it should "pass operations tests" in {
+  it should "pass operations compilation tests" in {
     val a = SCD(1.1, 2.2)
     val b = SCD.polar(1.6, math.Pi / 3)
 
@@ -79,6 +80,33 @@ final class SpireComplexDoubleApiUnit extends FlatSpec {
     assertCompiles("cosh(a)")
     assertCompiles("a.tanh")
     assertCompiles("tanh(a)")
+  }
+
+  it should "pass operations equivalence tests" in {
+    val a = SCD(1.1, 2.2)
+    val b = SCD.polar(1.6, math.Pi / 3)
+
+    assert(a + b == SD.add(a, b))
+    assert(a - b == SD.subtract(a, b))
+    assert(a * 3 == SD.integerMultiply(a, 3))
+    assert(a * b == SD.multiply(a, b))
+    assert(a / b == SD.divide(a, b))
+    assert(a ** 3 == SD.integerPower(a, 3))
+    assert(a.exp == SD.exp(a))
+    assert(a.sqrt == SD.sqrt(a))
+    assert(a.pow(b) == SD.pow(a, b))
+    assert(a.log == SD.log(a))
+    assert(a.sin == SD.sin(a))
+    assert(a.cos == SD.cos(a))
+    assert(a.tan == SD.tan(a))
+    assert(a.asin == SD.arcsin(a))
+    assert(a.acos == SD.arccos(a))
+    assert(a.atan == SD.arctan(a))
+    assert(a.sinh == SD.sinh(a))
+    assert(a.cosh == SD.cosh(a))
+    assert(a.tanh == SD.tanh(a))
+    assert((a - b).abs == SD.absDiff(a, b))
+    assert((a - b).abs == SD.absDiff(b, a))
   }
 
 }
