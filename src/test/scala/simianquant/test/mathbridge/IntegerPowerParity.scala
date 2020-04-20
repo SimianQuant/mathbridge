c889b0aa170595340fa14552f56ff12fef4b07d0
Harshad Deo <harshad@simianquant.com>
2020-04-20 15:33:03 +0530

    reinstate integer power parity and normal quantile cdf parity

--- src/test/scala/simianquant/test/mathbridge/IntegerPowerParity.scala
+++ src/test/scala/simianquant/test/mathbridge/IntegerPowerParity.scala
@@ -1,74 +1,69 @@
-// package simianquant.test.mathbridge
+package simianquant.test.mathbridge
 
-// import org.scalacheck.Gen
-// import org.scalatest.prop.Configuration
-// import org.scalacheck.Prop.forAll
-// import org.scalactic.anyvals.PosInt
-// import org.scalatest.PropSpec
-// import simianquant.mathbridge.IntegerPower
+import org.scalacheck.{Gen, Properties}
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Test.Parameters
+import simianquant.mathbridge.IntegerPower
 
-// /** Checks that IntegerPower is conformant with math.pow
-//   *
-//   * @author Harshad Deo
-//   * @since 0.1.0
-//   */
-// final class IntegerPowerParity extends PropSpec with Configuration {
+/** Checks that IntegerPower is conformant with math.pow
+  *
+  * @author Harshad Deo
+  * @since 0.1.0
+  */
+final class IntegerPowerParity extends Properties("IntegerPower"){
 
-//   implicit override final val generatorDrivenConfig = PropertyCheckConfiguration(
-//     minSuccessful = TestConstants.IntegerPowerRunCount,
-//     workers = PosInt.from(Runtime.getRuntime().availableProcessors()).get)
+      override def overrideParameters(p: Parameters) = 
+    p.withMinSuccessfulTests(TestConstants.IntegerPowerRunCount).withWorkers(Runtime.getRuntime().availableProcessors())
+  
+  val genDoubleCases = for {
+    base <- Gen.choose(-10.0, 10.0)
+    pow <- Gen.choose(-10, 10)
+  } yield (base, pow)
 
-//   val genDoubleCases = for {
-//     base <- Gen.choose(-10.0, 10.0)
-//     pow <- Gen.choose(-10, 10)
-//   } yield (base, pow)
+  val genFloatCases = for {
+    base <- Gen.choose(-10f, 10f)
+    pow <- Gen.choose(-8, 8)
+  } yield (base, pow)
 
-//   val genFloatCases = for {
-//     base <- Gen.choose(-10f, 10f)
-//     pow <- Gen.choose(-8, 8)
-//   } yield (base, pow)
+  property("double power parity") = {
+      forAll(genDoubleCases) {
+        case (base, exp) if base == 0d && exp <= 0 => true
+        case (base, exp) =>
+          val computed = IntegerPower.doublePower(base, exp)
+          val expected = math.pow(base, exp)
+          if (Preconditions.doubleUndefined(computed) || Preconditions.doubleUndefined(expected)) {
+            true
+          } else {
+            if (math.abs(expected) < 1) {
+              math.abs(computed - expected) < 1e-10
+            } else {
+              val err = math.abs((computed - expected) / expected)
+              err < 1e-10
+            }
+          }
+      }
+    
+  }
 
-//   property("double power parity") {
-//     check(
-//       forAll(genDoubleCases) {
-//         case (base, exp) if base == 0d && exp <= 0 => true
-//         case (base, exp) =>
-//           val computed = IntegerPower.doublePower(base, exp)
-//           val expected = math.pow(base, exp)
-//           if (Preconditions.doubleUndefined(computed) || Preconditions.doubleUndefined(expected)) {
-//             true
-//           } else {
-//             if (math.abs(expected) < 1) {
-//               math.abs(computed - expected) < 1e-10
-//             } else {
-//               val err = math.abs((computed - expected) / expected)
-//               err < 1e-10
-//             }
-//           }
-//       }
-//     )
-//   }
+  property("float power parity") = 
+      forAll(genFloatCases) {
+        case (base, exp) if base == 0f && exp <= 0 => true
+        case (base, exp) =>
+          val computed = IntegerPower.floatPower(base, exp)
+          val expected = math.pow(base, exp).toFloat
+          if (Preconditions.floatUndefined(computed) || Preconditions.floatUndefined(expected)) {
+            true
+          } else {
+            if (math.abs(expected) < 1) {
+              math.abs(computed - expected) < 1e-6
+            } else {
+              val err = math.abs((computed - expected) / expected)
+              err < 1e-6
+            }
 
-//   property("float power parity") {
-//     check(
-//       forAll(genFloatCases) {
-//         case (base, exp) if base == 0f && exp <= 0 => true
-//         case (base, exp) =>
-//           val computed = IntegerPower.floatPower(base, exp)
-//           val expected = math.pow(base, exp).toFloat
-//           if (Preconditions.floatUndefined(computed) || Preconditions.floatUndefined(expected)) {
-//             true
-//           } else {
-//             if (math.abs(expected) < 1) {
-//               math.abs(computed - expected) < 1e-6
-//             } else {
-//               val err = math.abs((computed - expected) / expected)
-//               err < 1e-6
-//             }
+          }
+      }
+    
+  
 
-//           }
-//       }
-//     )
-//   }
-
-// }
+}
--- src/test/scala/simianquant/test/mathbridge/NormalQuantileCdfParity.scala
+++ src/test/scala/simianquant/test/mathbridge/NormalQuantileCdfParity.scala
@@ -1,34 +1,30 @@
-// package simianquant.test.mathbridge
+package simianquant.test.mathbridge
 
-// import org.scalacheck.Gen
-// import org.scalatest.prop.Configuration
-// import org.scalacheck.Prop.forAll
-// import org.scalactic.anyvals.PosInt
-// import org.scalatest.PropSpec
-// import simianquant.mathbridge.NormalDistribution
+import org.scalacheck.{Gen, Properties}
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Test.Parameters
+import simianquant.mathbridge.NormalDistribution
 
-// final class NormalQuantileCdfParity extends PropSpec with Configuration {
+final class NormalQuantileCdfParity extends Properties("NormalQuantileCDF") {
 
-//   implicit override final val generatorDrivenConfig = PropertyCheckConfiguration(
-//     minSuccessful = TestConstants.IntegerPowerRunCount / 10,
-//     workers = PosInt.from(Runtime.getRuntime().availableProcessors()).get)
+    override def overrideParameters(p: Parameters) = 
+    p.withMinSuccessfulTests(TestConstants.IntegerPowerRunCount / 10).withWorkers(Runtime.getRuntime().availableProcessors())
+  private val _eps = 1e-10
 
-//   private val _eps = 1e-10
+  property("quantile(cdf)") = {
+    forAll(Gen.choose(-5.0, 5.0)) { x =>
+      val calc = NormalDistribution.quantile(NormalDistribution.cdf(x))
+      val delta = math.abs(x - calc)
+      delta < _eps
+    }
+  }
 
-//   property("quantile(cdf)") {
-//     check(forAll(Gen.choose(-5.0, 5.0)) { x =>
-//       val calc = NormalDistribution.quantile(NormalDistribution.cdf(x))
-//       val delta = math.abs(x - calc)
-//       delta < _eps
-//     })
-//   }
+  property("cdf(quantile)") =  
+    forAll(Gen.choose(_eps, 1 - _eps)) { x =>
+      val calc = NormalDistribution.cdf(NormalDistribution.quantile(x))
+      val delta = math.abs(x - calc)
+      delta < _eps
+    }
+  
 
-//   property("cdf(quantile)") {
-//     check(forAll(Gen.choose(_eps, 1 - _eps)) { x =>
-//       val calc = NormalDistribution.cdf(NormalDistribution.quantile(x))
-//       val delta = math.abs(x - calc)
-//       delta < _eps
-//     })
-//   }
-
-// }
+}
